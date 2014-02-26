96d367e04ecd2d0da6a73a8a70cd0a3db062446d
LeiNiuGatech <leiniugatech@gatech.edu>
2014-02-26 01:49:15 -0500

    L013

---
+++ 013/Form/From/Form/Form/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form myForm = new Form();
+            myForm.Text = "Hello World!";
+            myForm.Load += myForm_Load;
+            myForm.ShowDialog();
+        }
+
+        static void myForm_Load(object sender, EventArgs e)
+        {
+            Form form = sender as Form;
+            if (form == null) 
+            {
+                return;
+            }
+            form.Text = "Hello, New world!";
+        }
+    }
+}
