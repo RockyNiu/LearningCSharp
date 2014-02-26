de510537dabbc3bc94c203a1cf3ad198c1dabe93
Lei Niu (Rocky) <lei.niu.ny@gmail.com>
2014-02-26 16:58:05 -0500

    L014

---
+++ 014/ILDASM/ILDASM/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILDASM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = GetCylinderVolumn(10, 100);
+            Console.WriteLine(result);
+        }
+
+        static double GetCylinderVolumn(double r, double h)
+        {
+            double area = Math.PI * r * r;
+            double volumn = area * h;
+            return volumn;
+        }
+
+    }
+}
