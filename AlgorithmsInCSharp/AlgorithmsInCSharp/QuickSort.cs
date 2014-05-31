52a259fb6757c459e660112df899b5707ce58a21
RockyNiu <lei.niu.ny@gmail.com>
2014-05-30 22:06:41 -0400

    QuickSort and QuickSortTest

---
+++ AlgorithmsInCSharp/AlgorithmsInCSharp/QuickSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickSort quickSort = new QuickSort();
+            int[] A = { 1, 2, 4, 3, 5 };
+            int[] B = quickSort.Sort(A);
+            for (int i = 0; i < B.Length; i++)
+            {
+                Console.Write(B[i]+" ");
+            }
+        }
+    }
+
+    public class QuickSort
+    {
+
+        public int[] Sort(int[] A)
+        {
+            DoSorting(A, 0, A.Length - 1);
+            return A;
+        }
+
+        void DoSorting(int[] A, int left, int right)
+        {
+            if (right > left)
+            {
+                int pivot = Patition(A, left, right);
+                DoSorting(A, left, pivot - 1);
+                DoSorting(A, pivot + 1, right);
+            }
+        }
+
+        int Patition(int[] A, int left, int right)
+        {
+            Random random = new Random();
+            int pivot = left + random.Next(right - left + 1);
+            int pivotValue = A[pivot];
+            Swap(A, pivot, right);
+
+            int pivotNew = left;
+            for (int i = left; i < right; i++)
+            {
+                if (A[i] < pivotValue)
+                {
+                    Swap(A, pivotNew, i);
+                    pivotNew++;
+                }
+            }
+            Swap(A, pivotNew, right);
+            return pivotNew;
+        }
+
+        void Swap(int[] A, int a, int b)
+        {
+            int temp = A[a];
+            A[a] = A[b];
+            A[b] = temp;
+        }
+    }
+}
---
+++ AlgorithmsInCSharp/AlgorithmsInCSharpTest/QuickSortTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgorithmsInCSharp;
+
+namespace AlgorithmsInCSharpTest
+{
+
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void QuickSortTest_Empty()
+        {
+            int[] input = { };
+            int[] excepted = { };
+
+            int[] actual = (new QuickSort()).Sort(input);
+            CollectionAssert.AreEqual(excepted, actual);
+
+        }
+
+        [TestMethod]
+        public void QuickSortTest_OneItem()
+        {
+            int[] input = { 1 };
+            int[] excepted = { 1 };
+
+            int[] actual = (new QuickSort()).Sort(input);
+            CollectionAssert.AreEqual(excepted, actual);
+        }
+
+        [TestMethod]
+        public void QuickSortTest_TwoItems()
+        {
+            int[] input = { 2, 1 };
+            int[] excepted = { 1, 2 };
+
+            int[] actual = (new QuickSort()).Sort(input);
+            CollectionAssert.AreEqual(excepted, actual);
+        }
+
+        [TestMethod]
+        public void QuickSortTest_Random()
+        {
+            int[] input1 = { 2, 3, 1, 5, 4 };
+            int[] excepted1 = { 1, 2, 3, 4, 5 };
+
+            int[] input2 = { 2, 3, 4, 1, 3 };
+            int[] excepted2 = { 1, 2, 3, 3, 4 };
+
+            int[] actual1 = (new QuickSort()).Sort(input1);
+            CollectionAssert.AreEqual(excepted1, actual1);
+
+            int[] actual2 = (new QuickSort()).Sort(input2);
+            CollectionAssert.AreEqual(excepted2, actual2);
+        }
+
+        [TestMethod]
+        public void QuickSortTest_Ascending()
+        {
+            int[] input1 = { 1, 2, 3, 4, 5 };
+            int[] excepted1 = { 1, 2, 3, 4, 5 };
+
+            int[] input2 = { 1, 2, 3, 3, 4 };
+            int[] excepted2 = { 1, 2, 3, 3, 4 };
+
+            int[] actual1 = (new QuickSort()).Sort(input1);
+            CollectionAssert.AreEqual(excepted1, actual1);
+
+            int[] actual2 = (new QuickSort()).Sort(input2);
+            CollectionAssert.AreEqual(excepted2, actual2);
+        }
+
+        [TestMethod]
+        public void QuickSortTest_Descending()
+        {
+            int[] input1 = { 5, 4, 3, 2, 1 };
+            int[] excepted1 = { 1, 2, 3, 4, 5 };
+
+            int[] input2 = { 4, 3, 3, 2, 1 };
+            int[] excepted2 = { 1, 2, 3, 3, 4 };
+
+            int[] actual1 = (new QuickSort()).Sort(input1);
+            CollectionAssert.AreEqual(excepted1, actual1);
+
+            int[] actual2 = (new QuickSort()).Sort(input2);
+            CollectionAssert.AreEqual(excepted2, actual2);
+        }
+    }
+}
